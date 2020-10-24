169b02eb0340b0b37c7c2b3f93e0cda2a6e6bca4
a.savanovich <asavanster@gmail.com>
2020-10-24 06:59:13 +0300

    get url from clipboard if videos.txt is empty

--- tomp3.js
+++ tomp3.js
@@ -1,7 +1,9 @@
 const spawnAsync = require('@expo/spawn-async');
 const fs = require('fs');
 const readline = require('readline');
+const ncp = require("copy-paste");
 
+let counter = 0;
 const rl = readline.createInterface({
     input: fs.createReadStream('videos.txt'),
     output: process.stdout,
@@ -9,27 +11,40 @@ const rl = readline.createInterface({
 });
 
 async function processOneLink(link) {
-    const trimmedLink = link.trim();
-    if (!trimmedLink) {
-        return;
+    try {
+        const trimmedLink = link.trim();
+        if (!trimmedLink) {
+            return;
+        }
+
+        const myURL = new URL(trimmedLink);
+        const vParam = myURL.searchParams.get('v');
+        let newUrl = trimmedLink.split("?").shift()
+        if (vParam) {
+            const newSearchParams = new URLSearchParams();
+            newSearchParams.append('v', vParam);
+            const newUrlUrl = new URL(newUrl);
+            newUrlUrl.search = newSearchParams;
+            newUrl = newUrlUrl.href;
+        }
+        console.log(newUrl);
+        const resultPromise = spawnAsync('youtube-dl', ['-x', '--audio-format', 'mp3', newUrl], {stdio: 'inherit'});
+        await resultPromise;
+    } catch (e) {
+        console.log(e);
     }
-    const myURL = new URL(trimmedLink);
-    const vParam = myURL.searchParams.get('v');
-    let newUrl = trimmedLink.split("?").shift()
-    if (vParam) {
-        const newSearchParams = new URLSearchParams();
-        newSearchParams.append('v', vParam);
-        const newUrlUrl = new URL(newUrl);
-        newUrlUrl.search = newSearchParams;
-        newUrl = newUrlUrl.href;
-    }
-    console.log(newUrl);
-    const resultPromise = spawnAsync('youtube-dl', ['-x', '--audio-format', 'mp3', newUrl], { stdio: 'inherit' });
-    await resultPromise;
 }
 
 async function main() {
+
     for await (const line of rl) {
+        await processOneLink(line);
+        ++counter;
+    }
+
+    // TODO
+    if (counter === 0) {
+        const line = ncp.paste();
         await processOneLink(line);
     }
 }
