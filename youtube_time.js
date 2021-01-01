ef390a305fe0be5b6ea89a82425c4f36040ad216
a.savanovich <asavanster@gmail.com>
2021-01-01 13:57:38 +0300

    remove unused

--- youtube_time.js
+++ youtube_time.js
@@ -2,49 +2,38 @@
 const fs = require('fs');
 const readline = require('readline');
 
-let counter = 0;
-const rl = readline.createInterface({
-    input: fs.createReadStream('timing.txt'),
-    output: process.stdout,
-    terminal: false
-});
-
-function parseTimeLine(line) {
-    const a = line.split(':'); // split it at the colons
-    if (a.length < 2) {
-        return 0;
-    }
-    if (a.length === 2) {
-        return (+a[0]) * 60 + (+a[1]);
-    }
-
-// minutes are worth 60 seconds. Hours are worth 60 minutes.
-    return (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+function createFileReader(filename) {
+    return readline.createInterface({
+        input: fs.createReadStream(filename),
+        output: process.stdout,
+        terminal: false
+    });
 }
 
 function hmsToSecondsOnly(str) {
-    var p = str.split(':'),
-        s = 0, m = 1;
+    const p = str.split(':');
+    let s = 0;
+    let m = 1;
 
     while (p.length > 0) {
-        s += m * parseInt(p.pop(), 10) || 0;
+        s += m * parseInt(p.pop(), 10);
         m *= 60;
     }
 
     return s;
 }
 
-function secToString(SECONDS) {
-    const date = new Date(null);
-    date.setSeconds(SECONDS); // specify value for SECONDS here
-    return  date.toISOString();
-}
+const secToString = (sec) => new Date(sec*1000).toISOString();
 
 async function main() {
-
     let summ = 0;
+    let counter = 0;
+    const rl = createFileReader(process.argv[2] || 'timing.txt');
     for await (const line of rl) {
-        summ += hmsToSecondsOnly(line);
+        const sec = hmsToSecondsOnly(line);
+        if (sec) {
+           summ += sec;
+        }
         ++counter;
     }
     console.log(secToString(summ));
